59f5145aa1bbe0584c4b800ae0866ba89f86ecc2
cod-OR <rol.19u10407@btech.nitdgp.ac.in>
2021-08-27 18:31:52 +0530

    db changed to mysql

--- RECtangle/settings.py
+++ RECtangle/settings.py
@@ -82,12 +82,23 @@ WSGI_APPLICATION = 'RECtangle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': str(BASE_DIR.joinpath('my.cnf'))
+        },
     }
 }
+
 
 
 # Password validation
