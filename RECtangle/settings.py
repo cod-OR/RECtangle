b2f5b4ccab493d5c9fbd07c00f493da0250b9dbe
onkar <ratnaparkhionkar@gmail.com>
2022-01-08 15:51:22 +0530

    sqlite, local installs

--- RECtangle/settings.py
+++ RECtangle/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from .secret import *
+# from .secret import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'j^*6gi1fdbt)(akm-erutlr3v!t$f!f%y6ff5s7z5oeofv@05k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'RECtangle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': str(BASE_DIR.joinpath('my.cnf'))
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': str(BASE_DIR.joinpath('my.cnf'))
+#         },
+#     }
+# }
 
 
 
--- posts/views.py
+++ posts/views.py
@@ -11,18 +11,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class IsAllowed(LoginRequiredMixin, UserPassesTestMixin):
 	def test_func(self):
-		return self.request.user.staff					  #admins can change staff status of any user to revoke his/her access to the site
+		return self.request.user.staff
+		#admins can change staff status of any user to revoke his/her access to the site
 
 class HomePageView(IsAllowed, ListView):
 	model =Problem
 	template_name = 'home.html'
-	context_object_name = 'all_prob_list'				# home page shows a list of all current problems
+	context_object_name = 'all_prob_list'
+	# home page shows a list of all current problems
 
 class CreateProbView(IsAllowed, CreateView):
 	model = Problem
 	template_name = 'problem_create.html'
 	form_class=ProposeForm
-	
+
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super(CreateProbView, self).form_valid(form)
@@ -31,7 +33,8 @@ class CreateProbView(IsAllowed, CreateView):
 class EditProbView(IsAllowed, UpdateView):
 	model = Problem
 	template_name = 'problem_edit.html'
-	form_class=EditForm							# Edit form has a new field of status, to change status of the problem
+	form_class=EditForm
+	# Edit form has a new field of status, to change status of the problem
 
 
 class ProbDetailView(IsAllowed, FormMixin , DetailView):
@@ -67,4 +70,5 @@ class ProbDetailView(IsAllowed, FormMixin , DetailView):
 class DeleteProbView(IsAllowed, DeleteView):
 	model = Problem
 	template_name = 'problem_delete.html'
-	success_url = reverse_lazy('home')			# goes to home page after deleting
+	success_url = reverse_lazy('home')
+	# goes to home page after deleting
